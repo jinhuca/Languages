bfed5fcd047ea2ef2b5d8969dae7973dff6d3d90
Jin Hu <jinhuca@gmail.com>
2025-10-09 22:04:34 -0400

    fds

--- C++/Rainer Grimm/CppDetails/C03/C0301/C0301.cpp
+++
@@ -1,6 +0,0 @@
-// C0301.cpp 
-import std;
-
-int main() {
-  std::cout << "Hello World!\n";
-}
---
+++ C++/Rainer Grimm/CppDetails/C03/C030101_Concepts/C030101_Concepts.cpp
@@ -0,0 +1,13 @@
+// C030101_Concepts.cpp 
+import std;
+
+template<typename T>
+concept Integral = std::is_integral<T>::value;
+Integral auto gcd(Integral auto a, Integral auto b) {
+  if(b == 0) return a;
+  else return gcd(b, a % b);
+}
+
+int main() {
+  std::cout << gcd(48, 18) << std::endl; // Outputs: 6
+}
