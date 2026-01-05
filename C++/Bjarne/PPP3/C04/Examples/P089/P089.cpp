c61cbe1acc2f49b6310282d207a5ae5e44064b83
Jin Hu <jinhuca@gmail.com>
2026-01-04 21:33:04 -0500

    22

--- C++/Bjarne/PPP3/C04/Examples/P089/P089.cpp
+++ C++/Bjarne/PPP3/C04/Examples/P089/P089.cpp
@@ -14,20 +14,18 @@ int framed_area(int x, int y)
   return area(x - 2, y - 2);
 }
 
-void test(int x, int y, int z)
-{
+void test(int x, int y, int z) {
   int area1 = area(x, y);
   int area2 = framed_area(1, z);
   int area3 = framed_area(y, z);
   double ratio = double(area1) / area3;     // convert to double to get floating-point division
 }
 
-int main()
-{
-  int x = -1, y = 2, z =3;
-  if (x <= 0)
+int main() {
+  int x = -1, y = 2, z = 3;
+  if(x <= 0)
     throw std::runtime_error("non-positive x");
-  if (y <= 0)
+  if(y <= 0)
     throw std::runtime_error("non-positive y");
 
   test(x, y, z);
