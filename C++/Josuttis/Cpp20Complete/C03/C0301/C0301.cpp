cfd6affbb1d6afb2bd13c092e27c88cfd973893e
Jin Hu <jinhuca@gmail.com>
2026-01-13 21:26:12 -0500

    123

--- C++/Josuttis/Cpp20Complete/C03/C0301/C0301.cpp
+++ C++/Josuttis/Cpp20Complete/C03/C0301/C0301.cpp
@@ -1,14 +1,33 @@
 // C0301.cpp 
 import std;
 
+// Function template to return the maximum of two values of any same type
 template<typename T>
-  requires (!std::is_pointer_v<T>)
 T maxValue(T a, T b) {
   return (a > b) ? a : b;
 }
 
+void demo1() {
+  int x = 10, y = 20;
+  std::cout << "Max of " << x << " and " << y << " is " << maxValue(x, y) << '\n';
+}
+
+void demo2() {
+  double p = 5.5, q = 3.3;
+  std::cout << "Max of " << p << " and " << q << " is " << maxValue(p, q) << '\n';
+}
+
+void demo3() {
+  char c1 = 'z';
+  char c2 = 'a';
+  char* pc1 = &c1;
+  char* pc2 = &c2;
+
+  std::cout << "Max of " << c1 << " and " << c2 << " is " << maxValue(pc1, pc2) << '\n';
+}
+
 int main() {
-  std::cout << maxValue(12, 16) << '\n';
-  std::cout << maxValue(3.14, 2.72) << '\n';
-  //std::cout << maxValue("3.14", "2.72") << '\n';
+  demo1();
+  demo2();
+  demo3();
 }
--- C++/Josuttis/Cpp20Complete/C03/C030101/maxValue.ixx
+++ C++/Josuttis/Cpp20Complete/C03/C030101/maxValue.ixx
@@ -7,3 +7,12 @@ export template<typename T>
 T maxValue(T a, T b) {
   return a > b ? a : b;
 }
+
+template<typename T>
+concept IsPointer = std::is_pointer_v<T>;
+
+export template<typename T>
+requires (!IsPointer<T>)
+T MinValue(T a, T b) {
+  return b > a ? a : b;
+}
