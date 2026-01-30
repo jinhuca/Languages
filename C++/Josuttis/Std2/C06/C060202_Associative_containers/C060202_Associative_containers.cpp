d8d281adfb3bd7d8349746e333761a820dad4aa2
BSCI\huj6 <jin.hu@bsci.com>
2026-01-30 17:14:06 -0500

    123

--- C++/Josuttis/Std2/C06/C060202_Associative_containers/C060202_Associative_containers.cpp
+++ C++/Josuttis/Std2/C06/C060202_Associative_containers/C060202_Associative_containers.cpp
@@ -1,6 +1,56 @@
 // C060202_Associative_containers.cpp 
 import std;
 
+void multiset_example() {
+  std::multiset<std::string> cities{
+    "Braunschweig", "Hanover", "Frankfurt", "New York",
+    "Chicago", "Toronto", "Paris", "Frankfurt"
+  };
+  for (const auto& elem : cities) {
+    std::cout << elem << ' ';
+  }
+  std::cout << '\n';
+
+  cities.insert({ "London", "Munich", "Hanover", "Braunschweig"});
+  for (const auto& elem : cities) {
+    std::cout << elem << ' ';
+  }
+  std::cout << '\n';
+}
+
+void multimap_example() {
+  std::multimap<int, std::string> coll {
+    { 5, "tagged" }, 
+    { 2, "a" }, 
+    { 1, "this" },
+    { 4, "of" }, 
+    { 6, "strings" }, 
+    { 1, "is" },
+    { 3, "multimap" }
+  };
+  for (const auto& elem : coll) {
+    std::cout << elem.second << ' ';
+  }
+}
+
+void unordered_multiset_example() {
+  std::unordered_multiset<std::string> cities{
+    "Braunschweig", "Hanover", "Frankfurt", "New York",
+    "Chicago", "Toronto", "Paris", "Frankfurt"
+  };
+  for (const auto& elem : cities) {
+    std::cout << elem << ' ';
+  }
+  std::cout << '\n';
+  cities.insert({ "London", "Munich", "Hanover", "Braunschweig" });
+  for (const auto& elem : cities) {
+    std::cout << elem << ' ';
+  }
+  std::cout << '\n';
+}
+
 int main() {
-  std::cout << "Hello World!\n";
+  //multiset_example();
+  //multimap_example();
+  unordered_multiset_example();
 }
