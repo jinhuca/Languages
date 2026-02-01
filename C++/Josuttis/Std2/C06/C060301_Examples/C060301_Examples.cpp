b138d2a843db07412ab1ca231907fc4f0e30056b
Jin Hu <jinhuca@gmail.com>
2026-02-01 00:13:59 -0500

    3212

--- C++/Josuttis/Std2/C06/C060301_Examples/C060301_Examples.cpp
+++ C++/Josuttis/Std2/C06/C060301_Examples/C060301_Examples.cpp
@@ -1,6 +1,20 @@
 // C060301_Examples.cpp 
 import std;
 
+void list_iterator_example() {
+  std::list<char> coll;       // list container for characters
+  // append elements from 'a' to 'z'
+  for(char c = 'a'; c < 'z'; ++c) {
+    coll.push_back(c);
+  }
+  // iterate over all elements and print them
+  std::list<char>::const_iterator pos;
+  for(pos = coll.cbegin(); pos != coll.cend(); ++pos) {
+    std::cout << *pos << ' ';
+  }
+  std::cout << "\n";
+}
+
 int main() {
-  std::cout << "Hello World!\n";
+  list_iterator_example();
 }
