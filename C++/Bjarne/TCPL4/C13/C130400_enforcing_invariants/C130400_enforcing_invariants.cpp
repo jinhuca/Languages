4412042e647fbc53f263ffa078f5543b4fbf0616
Jin Hu <jinhuca@gmail.com>
2026-03-19 19:15:24 -0400

    1232

--- C++/Bjarne/TCPL4/C13/C130400_enforcing_invariants/C130400_enforcing_invariants.cpp
+++ C++/Bjarne/TCPL4/C13/C130400_enforcing_invariants/C130400_enforcing_invariants.cpp
@@ -1,5 +1,27 @@
 // C130400_enforcing_invariants.cpp 
 #include <iostream>
+#include <string>
+#include <exception>
+
+using std::string;
+using std::runtime_error;
+
+constexpr int CURRENT_MODE = 0;
+constexpr int CURRENT_LEVEL = 0;
+
+namespace Assert {
+  enum class Mode { throw_, terminate_, ignore_ };
+  
+  constexpr Mode current_mode = static_cast<Mode>( CURRENT_MODE);
+  constexpr int current_level = CURRENT_LEVEL;
+  
+  constexpr int default_level = 1;
+
+  constexpr bool level(int n) { return n <= current_level; }
+  struct Error : runtime_error {
+    //Error(const string& p) : runtime_error(p) {}
+  };
+}
 
 int main() {
   std::cout << "Hello World!\n";
